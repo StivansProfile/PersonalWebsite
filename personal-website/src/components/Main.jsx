30f64fdcfd6f132c49ea4423dd104f0b183eb4b0
StevenSon12 <stivanstf55@gmail.com>
2023-02-15 00:34:53 +0000

    Started work on the landing page

--- personal-website/src/components/Main.jsx
+++ personal-website/src/components/Main.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import '../styles/Main.css';
+import githubImg from "../assets/github.png";
+import linkedInImg from "../assets/linkedin.png";
+import instagramImg from "../assets/instagram.png";
 
 export default function Main(){
     return(
-        <div></div>
+        <div className="main">
+            <div className="main-titles">
+                <h1>Hello there my name is Stivan</h1>
+                <h2>I am an aspiring software developer</h2>
+            </div>
+            <div className="socials">
+                <img src={githubImg}></img>
+                <img src={linkedInImg}></img>
+                <img src={instagramImg}></img>
+            </div>
+            <button id="main-but">Learn more...</button>
+        </div>
+        
     );
 }
