4da0914a394d8c5e2429aa3f71e3fa73db8be28f
StevenSon12 <stivanstf55@gmail.com>
2023-02-16 19:32:33 +0000

    started the projects section and began working on making the individual containers for the projects

--- personal-website/src/components/Projects.jsx
+++ personal-website/src/components/Projects.jsx
@@ -4,7 +4,31 @@ import '../styles/Projects.css';
 export default function Projects(){
     return(
         <div className="projects">
-            
+
+            <div className="projects-title">
+                <h1 id="projects-h1">Projects</h1>
+                <hr id="projects-hr"></hr>
+                <p id="projects-p">
+                Here are the projects that I am most proud of and the projects
+                that I think are worth checking out.   
+                </p>
+            </div>
+
+            <div className="projects-wrap">
+                
+                <div className="c-sharp-project">
+                </div>
+
+                <div className="c-sharp-project">
+                </div>
+
+                <div className="c-sharp-project">
+                </div>
+
+                <div className="c-sharp-project">
+                </div>
+            </div>
+
         </div>
     );
 }
